555b16129f4a752e6b7dc8faab1b5ed361e71404
Trevor Dodds <trevor.dodds@gmail.com>
2018-08-09 20:33:39 -0400

    Update gpu_elastic.py

--- elasticsearch/gpu_elastic.py
+++ elasticsearch/gpu_elastic.py
@@ -24,7 +24,7 @@ def get_gpu_data(gpu):
              "ecc.errors.uncorrected.aggregate.total","gpu_uuid","clocks.max.mem","clocks.max.sm","clocks.max.graphics"]
     dType = [str, str, str, float, str, int, int, int, int, int, int, int, int, float, int, int, int, int, int, int, str, int, int, int]
     try:
-        output = subprocess.check_output(["nvidia-smi", "-i", gpu, "--query-gpu=timestamp,name,pci.bus_id,driver_version,"
+        output = subprocess.check_output(["nvidia-smi", "-i", str(gpu), "--query-gpu=timestamp,name,pci.bus_id,driver_version,"
                                                         "pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,"
                                                         "utilization.gpu,utilization.memory,memory.total,memory.free,"
                                                         "memory.used,power.draw,gpu_serial,clocks.current.graphics,"
@@ -52,18 +52,19 @@ def get_gpu_apps(gpu):
     query = ["timestamp","gpu_name","gpu_bus_id","gpu_serial","gpu_uuid","pid","process_name","used_gpu_memory"]
     dType = [str, str, str, int, str, int, str, int]
     try:
-        output = subprocess.check_output(["nvidia-smi", "-i", gpu, "--query-compute-apps=timestamp,gpu_name,gpu_bus_id,"
-                                                        "gpu_serial,gpu_uuid,pid,process_name,used_gpu_memory"
+        output = subprocess.check_output(["nvidia-smi", "-i", str(gpu), "--query-compute-apps=timestamp,gpu_name,gpu_bus_id,"
+                                                        "gpu_serial,gpu_uuid,pid,process_name,used_gpu_memory",
                                           "--format=csv,nounits,noheader"]).rstrip()
-        list_strings = output.split(", ")
-        list_values = [t(x) for t, x in zip(dType, list_strings)]
-        zipped = dict(zip(query, list_values))
-        zipped['@timestamp'] = str(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
-        zipped['node'] = socket.gethostname().lower()
-        zipped['gpu_id'] = 'gpu-'+str(gpu)
-        pp = pprint.PrettyPrinter(indent=4)
-        #pp.pprint(zipped)
-        post_data(zipped)
+        if len(output) >= 1:
+            list_strings = output.split(", ")
+            list_values = [t(x) for t, x in zip(dType, list_strings)]
+            zipped = dict(zip(query, list_values))
+            zipped['@timestamp'] = str(datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+            zipped['node'] = socket.gethostname().lower()
+            zipped['gpu_id'] = 'gpu-'+str(gpu)
+            pp = pprint.PrettyPrinter(indent=4)
+            #pp.pprint(zipped)
+            post_data(zipped)
     except Exception as e:
         print "Error:  {0}".format(str(e))
         pass
@@ -91,9 +92,11 @@ def main(gpus):
 
 if __name__ == '__main__':
     try:
-        gpus = int(subprocess.check_output(["nvidia-smi", "-L", "|", "wc -l"]).rstrip())
+        gpus = int(subprocess.check_output(["nvidia-smi", "-L"]).rstrip())
+        gpus = len(gpus.split('\n'))
         #print "Found %s GPUs" % gpus
-    except:
+    except subprocess.CalledProcessError as e:
+        print e.output
         print "No GPUs Found"
         exit(1)
     try:
